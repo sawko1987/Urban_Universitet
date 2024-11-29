3fee027976f4feae9db574322135273dac5dcdf9
Biryukov_AS <sawko1987.s@gmail.com>
2024-11-29 12:50:57 +0300

    На проверку преподавателем

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,21 +1,21 @@
 import random
 
 from random import randrange
-ran_number = randrange(3,20,1)
+n = randrange(3,20,1)
 spin = []
-for number1 in (range(1,20)):
-    for number2 in range(2,20):
-        sum = (number1 + number2 )
-        sum2 = (str(number2) + (str(number1 )))
-        if sum2 in spin:
-             continue
-        if number1 == number2:
+for i in (range(1,n-1)):
+    for j in range(i+1,n):
+        if n<=j:
             continue
-        if ran_number % sum==0:
-            spin.append(str(number1) + str(number2))
-        if sum >= ran_number:
-            break
+        s = i+ j
+        if n%s == 0:
+            spin.append(str(i))
+            spin.append(str(j))
+        else:
+            continue
+
 result = "".join(spin)
-print (f'{ran_number}-{result}')
+print (f'{n}-{result}')
+
 
 
