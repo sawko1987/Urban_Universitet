b16bac568ee947a2275ca11187d23299e5484428
Biryukov_AS <sawko1987.s@gmail.com>
2024-12-09 12:32:16 +0300

    На проверку преподавателем

--- module_5/module _5_2.py
+++ module_5/module _5_2.py
@@ -1,15 +1,37 @@
 class House:
     def __init__(self, name, number_of_floors):
+        self.new_floor = None
         self.name = name
         self.number_of_floors = int(number_of_floors)
-        self.new_floor = None
 
-    def go_to(self, new_floor):
+    def go_to(self,new_floor):
         self.new_floor = new_floor
-        if int(self.new_floor) > self.number_of_floors or int(self.new_floor) < 0:
-            print('Такого этажа не существует')
+        if int(self.new_floor) < 0 or int(self.new_floor) > self.number_of_floors:
+            print('"Такого этажа не существует"')
         else:
-            print(f'Перемещаемся на этаж {self.new_floor}')
+           print(f'Проийдете на {self.new_floor} этаж')
 
-h1 = House(name="ЖК Горский", number_of_floors=18)
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name} Этажей:{self.number_of_floors}"
+
+
+
+
+
+
+
+h1 = House('ЖК Горский', 18)
+print(len(h1))
+print (str(h1))
+h2 = House('Домик в деревне', 2)
+print (str(h2))
+print(len(h2))
+h3 = House('ЖК Эльбрус', 30)
+print (str(h3))
+print(len(h3))
 h1.go_to(5)
+h2.go_to(10)
+h3.go_to(15)
