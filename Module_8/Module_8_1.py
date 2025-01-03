7191f5f96620deff839df4d273ebcfaa4cd5f8a5
sawko1987 <sawko1987.s@gmail.com>
2025-01-03 09:20:35 +0300

    Update Module_8_1.py

--- Module_8/Module_8_1.py
+++ Module_8/Module_8_1.py
@@ -1,5 +1,3 @@
-
-#
 def  add_everything_up(a,b):
     """ принимает a и b  которые могут быть как числами(int, float), так и строками(str).
      и складывает их
@@ -11,12 +9,8 @@ def  add_everything_up(a,b):
     else:
         return round(result,3)
 
-
-
-# Пример кода:
-#
 print(add_everything_up(123.456, 'строка'))
 
 print(add_everything_up('яблоко', 4215))
-#
+
 print(add_everything_up(123.456, 7))
