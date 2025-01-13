b1196f1a29f62476893eb23ae490afa7187fabcd
Александр <sawo1987.s@gmail.com>
2025-01-13 20:21:47 +0300

    На проверку преподавателем

--- Module_9_functional_pragramming/Module_9_3_Generator assemblies.py
+++ Module_9_functional_pragramming/Module_9_3_Generator assemblies.py
@@ -2,7 +2,5 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 first_result=(len(x)-len(y) for x, y in zip(first, second) if len(x)!=len(y))
 print(list(first_result))
-second_result=((len(first[x])==(len(second[y])) for x,y in range(len(first))))
-
-
+second_result=(len(first[x])==len(second[x]) for x in range(len(first)))
 print(list(second_result))
--- file.py
+++ file.py
@@ -1,9 +1,6 @@
-text = "ABCd"
-for k,i in enumerate(text,0):
-    for k2, j in enumerate(text,1):
-        if k>=k2 or k>= len(text):
-            continue
-        else:
-            print(text[k:k2])
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+for x in
 
 
