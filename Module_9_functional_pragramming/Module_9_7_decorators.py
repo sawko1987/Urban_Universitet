7d24ffb0157cacddb21049bb5433987ce09d6b14
Biryukov_AS <sawko1987.s@gmail.com>
2025-01-14 12:44:23 +0300

    На проверку преподавателем

---
+++ Module_9_functional_pragramming/Module_9_7_decorators.py
@@ -0,0 +1,21 @@
+def is_prime(func):
+    """Функция декоратор (is_prime), которая распечатывает "Простое", если результат 1ой функции будет простым числом и
+    "Составное" в противном случае."""
+    def wrapper(*args,**kwargs):
+        result_sum = func()
+        if result_sum % 2 == 0 or result_sum % 3==0 or result_sum % 5==0 or result_sum % 7==0:
+            return "Число составное"
+        else:
+            return result_sum
+    return wrapper
+
+
+@is_prime
+def sum_three(*args):
+    """Функция, которая складывает 3 числа """
+    return sum(args)
+
+
+
+result = sum_three(2,3,6)
+print(result)
--- file.py
+++ file.py
@@ -1,6 +1,30 @@
-first = ['Strings', 'Student', 'Computers']
-second = ['Строка', 'Урбан', 'Компьютер']
+# Домашнее задание по теме "Декораторы"
 
-for x in
+# Функция декоратор (is_prime), которая распечатывает "Простое",
+# если результат 1ой функции будет простым числом и "Составное" в противном случае.
+def is_prime(func):
+# внутренняя функция wrapper в is_prime
+    def wrapper(*args, ** kwargs):
+        result = func(*args, ** kwargs)
+        sum_ = sum(args)
+        k = 0
+        for i in range(2, sum_ // 2 + 1):
+            if sum_ % i == 0:
+                k = k +1
+        if k <= 0:
+            print('Простое')
+        else:
+            print('Составное')
+        return result
+    return wrapper
+
+# @is_prime - декоратор для функции sum_three
+@is_prime
+# Функция, которая складывает 3 числа (sum_three)
+def sum_three(*args):
+    return sum(args)
 
 
+
+result = sum_three(2, 3, 6)
+print(result)
