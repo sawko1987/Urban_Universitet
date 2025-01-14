095f8661e174840d1ca2d1553de5ccaa2a7e2705
sawko <sawko1987.s@gmail.com>
2025-01-14 20:14:21 +0300

    На проверку преподавателем

--- Module_9_functional_pragramming/Module_9_7_decorators.py
+++ Module_9_functional_pragramming/Module_9_7_decorators.py
@@ -1,12 +1,16 @@
+import math
 def is_prime(func):
     """Функция декоратор (is_prime), которая распечатывает "Простое", если результат 1ой функции будет простым числом и
     "Составное" в противном случае."""
     def wrapper(*args,**kwargs):
         result_sum = func(*args,**kwargs)
-        if result_sum % 2 == 0 or result_sum % 3==0 or result_sum % 5==0 or result_sum % 7==0:
-            return "Число составное"
-        else:
+        if result_sum < 2:
             return result_sum
+        for sum in range(2, math.isqrt(result_sum) + 1):
+            if result_sum % sum == 0:
+                return "Число является составным"
+        return result_sum
+
     return wrapper
 
 
