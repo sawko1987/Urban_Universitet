55dba25b18372526ecad0b85c05b1dfe12a550d3
Александр <sawo1987.s@gmail.com>
2025-01-19 08:56:44 +0300

    На проверку преподавателем

--- Module_10_multithreading/Module_1_Introduction to streams.py
+++ Module_10_multithreading/Module_1_Introduction to streams.py
@@ -12,29 +12,33 @@ def wite_words(word_count, file_name):
         for f in range(word_count):
             file.write (f"Какое то слово №{f}\n")
             time.sleep(0.1)
-        print(threading.current_thread())
+        print(threading.current_thread()) # печатает текущий поток
     print (f"завершена запись в {file_name}")
 
-
+start_time1 = time.time()
 ex1 = wite_words(10, "example1.txt")
 ex2 = wite_words(30, "example2.txt")
 ex3 = wite_words(200, "example3.txt")
 ex4 = wite_words(100, "example4.txt")
+stop_time1 = time.time()
+lead_time1 = stop_time1 - start_time1
+print(f"Время выполнения основного потока составило: {lead_time1}")
 
+thread5 = threading.Thread(target=wite_words,args=(10,"example5"))
+thread6 = threading.Thread(target=wite_words,args=(30,"example6"))
+thread7 = threading.Thread(target=wite_words,args=(200,"example7"))
+thread8 = threading.Thread(target=wite_words,args=(100,"example8"))
 
-ex5 = wite_words(10, "example5.txt")
-ex6 = wite_words(30, "example6.txt")
-ex7 = wite_words(200, "example7.txt")
-ex8 = wite_words(100, "example8.txt")
-thread5 = threading.Thread(target=ex5)
-thread6 = threading.Thread(target=ex6)
-thread7 = threading.Thread(target=ex7)
-thread8 = threading.Thread(target=ex8)
+thread5.start()
+thread6.start()
+thread7.start()
+thread8.start()
 
-thread.start(ex5)
-thread.start(ex6)
-thread.start(ex7)
-thread.start(ex8)
+thread5.join()
+thread6.join()
+thread7.join()
+thread8.join()
 
-
-
+stop_time2 = time.time()
+lead_time2 = stop_time2 - stop_time1
+print(f"Время выполнения дополнительных потоков составило: {lead_time2}")
---
+++ Module_6_nassledovanie_classov/Module_6_final_Attempt #2.py
@@ -0,0 +1,26 @@
+class Figure:
+    sides_count = 0
+
+    def __init__(self,__color,__sides, filed:bool ):
+        self.__color = __color
+        self.__sides = __sides
+        self.filled = filed
+
+
+        pass
+
+
+
+
+
+class Circle(Figure):
+    pass
+
+
+class Triangle(Figure):
+    pass
+
+
+class Cube(Figure):
+    pass
+
